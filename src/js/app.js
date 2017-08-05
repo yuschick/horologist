6748cefe9de14e9bb1b79b51d89ef58e557839fe
Daniel Yuschick <danyuschick@gmail.com>
2017-08-05 19:55:29 +0300

    initial build

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  "use strict"
-  const util = require('./util');
-  const HeaderWatch = require('./site-modules/HeaderWatch');
-  const DemoWatches = require('./site-modules/DemoWatches');
-  const ComplicationsNav = require('./site-modules/ComplicationsNav');
-  const DocsPage = require('./site-modules/DocsPage');
-})();
