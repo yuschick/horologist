51101e844aede48802204c446ddc2d1524abf70f
Daniel Yuschick <danyuschick@gmail.com>
2017-08-08 13:25:35 +0300

    docs done

---
+++ ticktock/src/components/AsideNav/AsideNav.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
+import './AsideNav.css';
+
+class AsideNav extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fixed: false,
+    };
+
+    this.formatHeader = this.formatHeader.bind(this);
+    this.scrollToView = this.scrollToView.bind(this);
+  }
+
+  formatHeader(header) {
+      return header.toLowerCase().replace('/', '-').replace(' ', '-');
+  }
+
+  scrollToView(id) {
+    document.getElementById(id).scrollIntoView();
+  }
+
+  render() {
+    let label;
+
+    return (
+      <section className={`aside-nav-container ${this.props.fixed ? 'is-fixed' : ''}`}>
+        <nav className='aside-nav'>
+          <ul className='main-menu'>
+            {
+              this.props.menu.map(item => {
+                label = this.formatHeader(item.header);
+
+                return (
+                  <li ref={group => this[label] = group} key={label} className={`menu-group ${item.active ? 'active' : ''}`} data-group={this.formatHeader(item.header)} onClick={() => {this.props.update(item.header);}}>
+                    <span className='group-header'>{item.header}</span>
+
+                    <ul className='sub-menu'>
+                      {
+                        item.items.map(subitem => {
+                          return (
+                            <li key={this.formatHeader(subitem.label)} onClick={() => {
+                                this.scrollToView(subitem.anchor);
+                              }
+                            }>
+                              <Link to={`#${subitem.anchor}`}>{subitem.label}</Link>
+                            </li>
+                          )
+                        })
+                      }
+                    </ul>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </nav>
+      </section>
+    );
+  }
+}
+
+AsideNav.propTypes = {
+  menu: PropTypes.array.isRequired,
+  update: PropTypes.func.isRequired,
+  fixed: PropTypes.bool,
+}
+
+export default AsideNav;
--- ticktock/src/components/ComplicationContainer/ComplicationContainer.js
+++ ticktock/src/components/ComplicationContainer/ComplicationContainer.js
@@ -14,6 +14,8 @@ class ComplicationContainer extends Component {
 
   getDescription() {
     let desc;
+    let code;
+
     switch (this.props.id) {
       case 'dials':
         desc = [
@@ -30,6 +32,30 @@ class ComplicationContainer extends Component {
             text: 'Additionally, a dial supports sweeping and ticking seconds hands. The primary dial shows a ticking seconds hand, or deadbeat seconds, while the sub dial shows the seconds hand sweeping.',
           },
         ];
+
+        code = `let settings = {
+  dials: [{
+    name: 'primary',
+    hands: {
+      hour: 'dial-primary-hour-hand',
+      minute: 'dial-primary-minute-hand',
+      second: 'dial-primary-second-hand'
+    }
+  },
+  {
+    name: 'secondary',
+    hands: {
+      hour: 'dial-secondary-hour-hand',
+      minute: 'dial-secondary-minute-hand',
+      second: 'dial-secondary-second-hand'
+    },
+    offset: '+6',
+    sweep: true
+  }]
+};
+
+let demo = new Watch(settings);
+`;
         break;
       case 'perpetual-calendar':
         desc = [
@@ -42,6 +68,31 @@ class ComplicationContainer extends Component {
             text: `In the demo, the month and day abbreviations are shown as well as the current date. The year display, at 6 o${String.fromCharCode(39)}clock, shows a circle broken into quadrants where the highlighted quadrant indicates the leap year. From this display, we can conclude that 2020 will be the next leap year.`,
           },
         ];
+
+        code = `let settings = {
+  dials: [{
+    hands: {
+      hour: 'perpetual-hour-hand',
+      minute: 'perpetual-minute-hand',
+      second: 'perpetual-second-hand'
+    }
+  }],
+  day: {
+    id: 'day-indicator-disc'
+  },
+  date: {
+    id: 'date-disc'
+  },
+  month: {
+    id: 'month-disc'
+  },
+  year: {
+    id: 'year-hand'
+  }
+};
+
+let demo = new Watch(settings);
+`;
       break;
       case 'day-night-indicator':
         desc = [
@@ -54,6 +105,21 @@ class ComplicationContainer extends Component {
             text: `Many simple day/night indicators are a two-color disc that show "day" only from midnight to 6 AM and "night" only from noon to 6 PM, with a half-day or half-night display the rest of the time. TickTock follows this convention.`,
           },
         ];
+        code = `let settings = {
+  dials: [{
+    hands: {
+      hour: 'day-night-hour-hand',
+      minute: 'day-night-minute-hand',
+      second: 'day-night-second-hand'
+    }
+  }],
+  dayNightIndicator: {
+    id: 'day-night-dial'
+  }
+};
+
+let demo = new Watch(settings);
+`;
       break;
       case 'manual-time':
         desc = [
@@ -66,6 +132,31 @@ class ComplicationContainer extends Component {
             text: `Additionally, if a secondary dial is being used, while in the set state, the right arrow key will allow a user to target the secondary dial to set its time independently.`,
           },
         ];
+        code = `let settings = {
+  dials: [{
+      name: 'primary',
+      hands: {
+        hour: 'crown-primary-hour-hand',
+        minute: 'crown-primary-minute-hand',
+        second: 'crown-primary-second-hand'
+      }
+    },
+    {
+      name: 'secondary',
+      hands: {
+        hour: 'crown-secondary-hour-hand',
+        minute: 'crown-secondary-minute-hand'
+      },
+      offset: '+6'
+    }
+  ],
+  crown: {
+    id: 'the-crown'
+  }
+};
+
+let demo = new Watch(settings);
+`;
       break;
       case 'minute-repeater':
         desc = [
@@ -78,6 +169,26 @@ class ComplicationContainer extends Component {
             text: `TickTock follows the convention of chiming for every hour, quarter hour, and single minute. It defaults to indicating the time of the first dial passed into the dials array but can be set to read a secondary dial as well. And the trigger for the repeater can be any element (SVG or otherwise) so long as its ID is passed into the settings.`,
           },
         ];
+        code = `let settings = {
+  dials: [{
+    hands: {
+      hour: 'repeater-hour-hand',
+      minute: 'repeater-minute-hand',
+      second: 'repeater-second-hand'
+    }
+  }],
+  repeater: {
+    id: 'play-repeater',
+    chimes: {
+      hour: './dist/sounds/chime-01.mp4',
+      quarter: './dist/sounds/chime-02.mp4',
+      minute: './dist/sounds/chime-03.mp4'
+    }
+  }
+};
+
+let demo = new Watch(settings);
+`;
       break;
       case 'moonphase':
         desc = [
@@ -90,6 +201,21 @@ class ComplicationContainer extends Component {
             text: `TickTock will rotate the moonphase disc from new moon to waxing crescent, first quarter, waxing gibbous, full moon, waning gibbous, three quarter, and waning crescent. It will also default to rotating the disc clockwise beginning from the full moon position which can be reversed with the 'invert' property.`,
           },
         ];
+        code = `let settings = {
+  dials: [{
+    hands: {
+      hour: 'moonphase-hour-hand',
+      minute: 'moonphase-minute-hand',
+      second: 'moonphase-second-hand'
+    }
+  }],
+  moonphase: {
+    id: 'moonphase-dial'
+  }
+};
+
+let demo = new Watch(settings);
+`;
       break;
       case 'power-reserve':
         desc = [
@@ -106,19 +232,35 @@ class ComplicationContainer extends Component {
             text: `Note that the range values are relative to the hand's starting position.`,
           },
         ];
+        code = `let settings = {
+  dials: [{
+    hands: {
+      hour: 'reserve-hour-hand',
+      minute: 'reserve-minute-hand',
+      second: 'reserve-second-hand'
+    }
+  }],
+  reserve: {
+    id: 'reserve-hand',
+    range: [-90, 90]
+  }
+};
+
+let demo = new Watch(settings);
+`;
       break;
       default:
         return false;
     }
 
-    return desc;
+    return {desc, code};
   }
   render() {
     return (
       <section className={`complication-container flex-container is-spread ${!this.props.active ? 'is-hidden' : false}`} data-comp={this.props.id}>
         <div className='flex-one'>
           {
-            this.getDescription().map(para => {
+            this.getDescription().desc.map(para => {
               return (
                 <p key={para.key}>{para.text}</p>
               );
@@ -129,7 +271,9 @@ class ComplicationContainer extends Component {
             <Button type='primary view-code-btn' text='Toggle Code' url='#' data={this.props.demo} action={this.props.toggle} />
           </div>
           <section className={`complication code-block-container ${!this.props.showCode ? 'is-hidden' : ''}`} data-type={this.props.demo}>
-            <CodeBlock id={this.props.id} />
+            <CodeBlock>
+              {this.getDescription().code}
+            </CodeBlock>
           </section>
         </div>
         <div className='complication-demo flex-one'>
