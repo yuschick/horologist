206b421aa2fa927e2b678f4a23212a2da1a644d8
Daniel Yuschick <danyuschick@gmail.com>
2017-10-24 08:56:41 +0300

    App update

--- dist/js/app.js
+++ dist/js/app.js
@@ -345,10 +345,10 @@
 	var DayIndicator = __webpack_require__(132);
 	var DateIndicator = __webpack_require__(133);
 	var MonthIndicator = __webpack_require__(134);
-	var WeekIndicator = __webpack_require__(138);
-	var YearIndicator = __webpack_require__(135);
-	var Chronograph = __webpack_require__(136);
-	var Foudroyante = __webpack_require__(137);
+	var WeekIndicator = __webpack_require__(135);
+	var YearIndicator = __webpack_require__(136);
+	var Chronograph = __webpack_require__(137);
+	var Foudroyante = __webpack_require__(138);
 
 	var Watch = function () {
 	    function Watch(settings) {
@@ -18650,6 +18650,83 @@
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+	// Week Indicator Class
+	// @params settings: object
+	// @params parentWatch: Watch instance
+	//
+	// The Week Class will rotate an element to depict the 
+	// current week of the year. By default, this class expects 
+	// 52 weeks per year but an iso boolean can be passed
+	// to account for a 53rd week. The week indicator
+	// element can rotate either direction based on the 
+	// invert boolean as well.
+
+	var WeekIndicator = function () {
+	    function WeekIndicator(settings, parentWatch) {
+	        _classCallCheck(this, WeekIndicator);
+
+	        this.errorChecking();
+
+	        this.element = document.getElementById(settings.id);
+	        this.parent = parentWatch;
+	        this.iso = settings.iso || false;
+	        this.invert = settings.invert || false;
+
+	        this.week = 0;
+	        this.weekAmount = this.iso ? 53 : 52;
+	        this.increment = 360 / this.weekAmount;
+
+	        this.init();
+	    }
+
+	    _createClass(WeekIndicator, [{
+	        key: "errorChecking",
+	        value: function errorChecking() {
+	            try {
+	                if (!settings.id) throw "The Week Indicator class requires that an ID of the indicator element be provided.";
+	            } catch (errorMsg) {
+	                console.error(errorMsg);
+	                return;
+	            }
+	        }
+	    }, {
+	        key: "getWeekValue",
+	        value: function getWeekValue() {
+	            var rightNow = this.parent.rightNow;
+	            this.week = this.iso ? rightNow.isoWeek() - 1 : rightNow.week() - 1;
+
+	            this.rotateHands();
+	        }
+	    }, {
+	        key: "rotateHands",
+	        value: function rotateHands() {
+	            var rotateVal = this.week * this.increment;
+	            if (this.invert) rotateVal *= -1;
+
+	            this.element.style.transform = "rotate(" + rotateVal + "deg)";
+	        }
+	    }, {
+	        key: "init",
+	        value: function init() {
+	            this.getWeekValue();
+	        }
+	    }]);
+
+	    return WeekIndicator;
+	}();
+
+	module.exports = WeekIndicator;
+
+/***/ }),
+/* 136 */
+/***/ (function(module, exports) {
+
+	"use strict";
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 	// YearIndicator Class
 	// @params settings: object
 	// @params parentWatch: Watch instance
@@ -18718,7 +18795,7 @@
 	module.exports = YearIndicator;
 
 /***/ }),
-/* 136 */
+/* 137 */
 /***/ (function(module, exports) {
 
 	"use strict";
@@ -19042,7 +19119,7 @@
 	module.exports = Chronograph;
 
 /***/ }),
-/* 137 */
+/* 138 */
 /***/ (function(module, exports) {
 
 	"use strict";
@@ -19151,83 +19228,6 @@
 	}();
 
 	module.exports = Foudroyante;
-
-/***/ }),
-/* 138 */
-/***/ (function(module, exports) {
-
-	"use strict";
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	// Week Indicator Class
-	// @params settings: object
-	// @params parentWatch: Watch instance
-	//
-	// The Week Class will rotate an element to depict the 
-	// current week of the year. By default, this class expects 
-	// 52 weeks per year but an iso boolean can be passed
-	// to account for a 53rd week. The week indicator
-	// element can rotate either direction based on the 
-	// invert boolean as well.
-
-	var WeekIndicator = function () {
-	    function WeekIndicator(settings, parentWatch) {
-	        _classCallCheck(this, WeekIndicator);
-
-	        this.errorChecking();
-
-	        this.element = document.getElementById(settings.id);
-	        this.parent = parentWatch;
-	        this.iso = settings.iso || false;
-	        this.invert = settings.invert || false;
-
-	        this.week = 0;
-	        this.weekAmount = this.iso ? 53 : 52;
-	        this.increment = 360 / this.weekAmount;
-
-	        this.init();
-	    }
-
-	    _createClass(WeekIndicator, [{
-	        key: "errorChecking",
-	        value: function errorChecking() {
-	            try {
-	                if (!settings.id) throw "The Week Indicator class requires that an ID of the indicator element be provided.";
-	            } catch (errorMsg) {
-	                console.error(errorMsg);
-	                return;
-	            }
-	        }
-	    }, {
-	        key: "getWeekValue",
-	        value: function getWeekValue() {
-	            var rightNow = this.parent.rightNow;
-	            this.week = this.iso ? rightNow.isoWeek() - 1 : rightNow.week() - 1;
-
-	            this.rotateHands();
-	        }
-	    }, {
-	        key: "rotateHands",
-	        value: function rotateHands() {
-	            var rotateVal = this.week * this.increment;
-	            if (this.invert) rotateVal *= -1;
-
-	            this.element.style.transform = "rotate(" + rotateVal + "deg)";
-	        }
-	    }, {
-	        key: "init",
-	        value: function init() {
-	            this.getWeekValue();
-	        }
-	    }]);
-
-	    return WeekIndicator;
-	}();
-
-	module.exports = WeekIndicator;
 
 /***/ })
 /******/ ]);
