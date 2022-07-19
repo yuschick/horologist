ed7016b3890ee7cd984adc8050d9376066f42c97
Daniel Yuschick <daniel.yuschick@gmail.com>
2022-07-19 21:51:56 +0300

    feat: support power reserve invert

--- src/modules/PowerReserve.js
+++ src/modules/PowerReserve.js
@@ -9,58 +9,71 @@
 // components, with the exception of the crown, are stopped.
 
 class PowerReserve {
-    constructor(settings, parentWatch) {
-        this.errorChecking(settings);
+  constructor(settings, parentWatch) {
+    this.errorChecking(settings);
 
-        this.element = document.getElementById(settings.id);
-        this.interval = null;
-        this.parent = parentWatch;
-        this.minAngle = settings.range[0];
-        this.maxAngle = settings.range[1];
-        this.increment = 0.5;
+    this.element = document.getElementById(settings.id);
+    this.interval = null;
+    this.parent = parentWatch;
+    this.minAngle = settings.range[0];
+    this.maxAngle = settings.range[1];
+    this.increment = 0.5;
+    this.invert = settings.invert || false;
 
-        if (!this.parent.testing) this.init();
+    if (!this.parent.testing) this.init();
+  }
+
+  errorChecking(settings) {
+    if (!settings.id)
+      throw new ReferenceError(
+        "The PowerReserve class requires that an ID of the power reserve element be provided."
+      );
+    if (!settings.range)
+      throw new ReferenceError(
+        "The PowerReserve class requires that a range of the power reserve element be provided."
+      );
+  }
+
+  decrementReserve() {
+    let currentRotate = this.parent.getCurrentRotateValue(this.element);
+
+    if (currentRotate <= this.minAngle) {
+      this.parent.stopInterval();
+    } else {
+      let newRotate = Number(currentRotate) - this.increment / 2;
+      if (this.invert) newRotate *= -1;
+      this.element.style.transform = `rotate(${newRotate}deg)`;
     }
+  }
 
-    errorChecking(settings) {
-        if (!settings.id) throw new ReferenceError('The PowerReserve class requires that an ID of the power reserve element be provided.');
-        if (!settings.range) throw new ReferenceError('The PowerReserve class requires that a range of the power reserve element be provided.');
+  incrementReserve() {
+    let currentRotate = this.parent.getCurrentRotateValue(this.element);
+
+    if (
+      currentRotate <= this.maxAngle - this.increment &&
+      currentRotate >= this.minAngle
+    ) {
+      let newRotate = Number(currentRotate) + this.increment;
+      if (this.invert) newRotate *= -1;
+      this.element.style.transform = `rotate(${newRotate}deg)`;
     }
+  }
 
-    decrementReserve() {
-        let currentRotate = this.parent.getCurrentRotateValue(this.element);
+  startInterval() {
+    this.interval = setInterval(() => {
+      this.decrementReserve();
+    }, 1000);
+  }
 
-        if (currentRotate <= this.minAngle) {
-            this.parent.stopInterval();
-        } else {
-            let newRotate = Number(currentRotate) - (this.increment / 2);
-            this.element.style.transform = `rotate(${newRotate}deg)`;
-        }
-    }
+  stopInterval() {
+    clearInterval(this.interval);
+    this.interval = null;
+  }
 
-    incrementReserve() {
-        let currentRotate = this.parent.getCurrentRotateValue(this.element);
-
-        if (currentRotate <= (this.maxAngle - this.increment) && currentRotate >= this.minAngle) {
-            let newRotate = Number(currentRotate) + this.increment;
-            this.element.style.transform = `rotate(${newRotate}deg)`;
-        }
-    }
-
-    startInterval() {
-        this.interval = setInterval(() => {
-            this.decrementReserve();
-        }, 1000);
-    }
-
-    stopInterval() {
-        clearInterval(this.interval);
-        this.interval = null;
-    }
-
-    init() {
-        this.element.style.transform = `rotate(${this.maxAngle}deg)`;
-    }
+  init() {
+    const initAngle = this.invert ? this.maxAngle * -1 : this.maxAngle;
+    this.element.style.transform = `rotate(${initAngle}deg)`;
+  }
 }
 
 module.exports = PowerReserve;
