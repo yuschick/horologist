417dd4e07d8e2319d7bcc76cb592cb2399817b4f
Daniel Yuschick <daniel.yuschick@gmail.com>
2022-07-20 16:49:32 +0300

    feat: set up base foudroyante component

---
+++ packages/ticktock-js/src/components/Foudroyante/Foudroyante.types.ts
@@ -0,0 +1,13 @@
+export interface FoudroyanteClass {
+    readonly errorChecking: () => void;
+    readonly init: () => void;
+    readonly clearInterval: () => void;
+    readonly rotate: () => void;
+    readonly startInterview: () => void;
+}
+
+export interface FoudroyanteOptions {
+    id: string;
+    reverse?: boolean;
+    steps: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+}
