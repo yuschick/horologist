82b630351847ea948b1b81e5626c438dce720e19
Daniel Yuschick <daniel.yuschick@gmail.com>
2022-07-20 20:34:40 +0300

    test: add appending test coverage to utils rotate

--- packages/ticktock-js/src/utils/index.test.ts
+++ packages/ticktock-js/src/utils/index.test.ts
@@ -2,10 +2,20 @@ import * as utils from './';
 
 describe('rotate', () => {
     const element = document.createElement('div');
+    const value = 30;
 
     it('should set the correct transform value', () => {
-        utils.rotate({ element, value: 30 });
+        utils.rotate({ element, value });
 
-        expect(element.style.transform).toEqual('rotate(30deg)');
+        expect(element.style.transform).toEqual(`rotate(${value}deg)`);
+    });
+
+    it('should append the correct transform value', () => {
+        const transform = 'scale(1.1)';
+        element.style.transform = transform;
+
+        utils.rotate({ element, value });
+
+        expect(element.style.transform).toEqual(`${transform} rotate(${value}deg)`);
     });
 });
