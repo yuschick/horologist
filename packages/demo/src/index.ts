4bc0022de77b5f39d806d3b549eb2db6423139b7
Daniel Yuschick <daniel.yuschick@gmail.com>
2022-07-21 11:45:01 +0300

    test: add month indicator to demo

--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -3,6 +3,7 @@ import { Watch } from 'ticktock-js';
 const test = new Watch({
     id: 'demo-watch',
     day: { id: 'day-id' },
+    month: { id: 'month-id', offsetDate: true, reverse: true },
     foudroyante: { steps: 5, id: 'foudroyante-id', reverse: true },
     year: { id: 'year-id', offsetMonths: true },
 });
