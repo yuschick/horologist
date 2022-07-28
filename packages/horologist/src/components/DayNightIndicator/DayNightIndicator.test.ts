1da44146d46acc9e3a35d733d08bf3781b08d4cd
Daniel Yuschick <daniel.yuschick@gmail.com>
2022-07-28 17:24:49 +0300

    test: trying to get gh tests to pass

--- packages/horologist/src/components/DayNightIndicator/DayNightIndicator.test.ts
+++ packages/horologist/src/components/DayNightIndicator/DayNightIndicator.test.ts
@@ -13,7 +13,7 @@ describe('Day/Night Indicator', () => {
         },
     ];
     const id = 'test-id';
-    const date = new Date('2022/7/20 13:30:10');
+    const date = new Date('2022/7/20 13:30:10 GMT+3:00');
     const rotateIncrement = 360 / 4;
     let test: Watch;
 
