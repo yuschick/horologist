751a81ae02cc302d261aa715f00fb43552ee2765
Daniel Yuschick <daniel.yuschick@gmail.com>
2022-08-04 11:10:42 +0300

    feat: base setup of site and landing page

---
+++ packages/docs/esbuild.config.js
@@ -0,0 +1,8 @@
+require('esbuild').buildSync({
+    entryPoints: ['./src/scripts/index.ts'],
+    bundle: true,
+    minify: true,
+    outdir: '_site/js',
+    external: [],
+    format: 'esm',
+});
