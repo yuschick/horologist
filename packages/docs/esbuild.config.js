1db18192decc7dc1fbffea15b6842f354103a6f9
Daniel Yuschick <daniel.yuschick@gmail.com>
2023-05-08 09:27:54 +0300

    chore: remove docs dirs for remix drop in

--- packages/docs/esbuild.config.js
+++
@@ -1,8 +0,0 @@
-require('esbuild').buildSync({
-    entryPoints: ['./src/scripts/index.ts'],
-    bundle: true,
-    minify: true,
-    outdir: '_site/js',
-    external: ['horologist'],
-    format: 'esm',
-});
