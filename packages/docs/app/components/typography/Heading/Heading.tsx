d06fca9d03508e481f5752bdf376c2d47ec7ee92
Daniel Yuschick <yuschick@users.noreply.github.com>
2023-05-09 09:33:22 +0300

    Use Dynamic Routes for Complications (#71)
    * refactor: use dynamic complication route
    
    * refactor: improve heading styles

--- packages/docs/app/components/typography/Heading/Heading.tsx
+++ packages/docs/app/components/typography/Heading/Heading.tsx
@@ -1,11 +1,11 @@
 import { generateThemeStyles } from '~/utils/generateThemeStyles';
 import classNames from 'classnames';
 
-import styles from '../Text/Text.module.css';
+import headingStyles from './Heading.module.css';
+import textStyles from '../Text/Text.module.css';
 import type { HeadingProps } from './Heading.types';
 import { Link } from '~/components/navigation/Link';
 import { Text } from '../Text';
-import { stringToKebab } from '~/utils/stringToKebab';
 
 export function Heading({
     appearAs,
@@ -13,22 +13,21 @@ export function Heading({
     isJumpToHeading,
     children,
     className,
+    id,
     lines,
     truncate,
     tx,
     ...htmlAttributes
 }: HeadingProps) {
     const style = tx && generateThemeStyles({ tx });
-    const headingSlug = children && stringToKebab(children.toString());
 
     return (
         <Element
             className={classNames(className, {
-                [styles['is-jump-to-heading']]: isJumpToHeading,
-                [styles['is-truncated']]: truncate,
-                [styles['with-lines-limit']]: lines,
+                [headingStyles['is-jump-to-heading']]: isJumpToHeading,
+                [textStyles['is-truncated']]: truncate,
+                [textStyles['with-lines-limit']]: lines,
             })}
-            {...(headingSlug ? { id: headingSlug } : {})}
             style={{
                 ...style,
                 ...(lines ? { '--lines': lines } : {}),
@@ -40,9 +39,9 @@ export function Heading({
             {isJumpToHeading && (
                 <Link
                     aria-hidden="true"
-                    className={styles['can-be-copied-link']}
+                    className={headingStyles['can-be-copied-link']}
                     tabIndex={-1}
-                    to={`./#${headingSlug}`}
+                    to={`./#${id}`}
                 >
                     <Text />
                 </Link>
--- packages/docs/app/routes/docs.complications.$complication.tsx
+++ packages/docs/app/routes/docs.complications.$complication.tsx
@@ -1,16 +1,22 @@
 import type { V2_MetaFunction } from '@remix-run/node';
+import { Params, useParams } from '@remix-run/react';
 import { Heading } from '~/components/typography/Heading/Heading';
 import { Text } from '~/components/typography/Text';
+import { kebabToTitle } from '~/utils/stringUtils';
 
 // TODO: Add og tags to meta function
-export const meta: V2_MetaFunction = () => {
-    return [{ title: 'Chronograph - Horologist' }];
+export const meta: V2_MetaFunction = ({ params }) => {
+    const prettyTitle = kebabToTitle(params.complication || '');
+    return [{ title: `${prettyTitle} - Horologist` }];
 };
 
-export default function DocsComplicationChronograph() {
+export default function DocsComplications() {
+    const { complication = '' } = useParams();
+    const prettyHeading = kebabToTitle(complication);
+
     return (
         <>
-            <Heading>Chronograph</Heading>
+            <Heading>{prettyHeading}</Heading>
             <Text as="p">
                 An analog watch containing a stopwatch function. Dedicated hand can be started,
                 stopped and reset via pushers on the case, with varying amounts of separate
